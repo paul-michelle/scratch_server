97f2274584603fed14f7040d0269343c705d7cae
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-10-30 21:13:12 +0300

    Add optional requests to serve limit

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,24 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = lib::ThreadPool::build(4).unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| {
-            lib::handle_connection(stream);
-        });
+    match std::env::args().nth(1) {
+        Some(requests_count) => {
+            let reqs_to_take: usize = requests_count.parse().unwrap();
+            for stream in listener.incoming().take(reqs_to_take) {
+                let stream = stream.unwrap();
+                pool.execute(|| {
+                    lib::handle_connection(stream);
+                });
+            }
+            println!("Shutting down.")
+        }
+        None => {
+            for stream in listener.incoming() {
+                let stream = stream.unwrap();
+                pool.execute(|| {
+                    lib::handle_connection(stream);
+                });
+            }
+        }
     }
-
-    println!("Shutting down.")
 }
