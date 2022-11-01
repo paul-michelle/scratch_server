3c0010584d5a73d73bf26aef32fed7521c9bf911
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-11-01 19:33:06 +0300

    Enable passing server address from cli

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,54 @@
 use scratch_server as lib;
 use std::net::TcpListener;
 
+struct Config {
+    server_addr: String,
+    requests_to_serve: Option<usize>,
+}
+
+impl Config {
+    fn new(mut args: impl Iterator<Item = String>) -> Self {
+        args.next();
+
+        let server_addr = match args.next() {
+            Some(arg) => arg,
+            None => String::from("127.0.0.1:7878"),
+        };
+        let requests_to_serve: Option<usize> = match args.next() {
+            Some(arg) => match arg.parse::<usize>() {
+                Ok(count) => Some(count),
+                Err(_) => None,
+            },
+            None => None,
+        };
+        Self {
+            server_addr,
+            requests_to_serve,
+        }
+    }
+}
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let config = Config::new(std::env::args());
+    let listener = TcpListener::bind(config.server_addr).unwrap();
     let pool = lib::ThreadPool::build(4).unwrap();
 
-    match std::env::args().nth(1) {
-        Some(requests_count) => {
-            let reqs_to_take: usize = requests_count.parse().unwrap();
-            for stream in listener.incoming().take(reqs_to_take) {
-                let stream = stream.unwrap();
-                pool.execute(|| {
-                    lib::handle_connection(stream);
-                });
-            }
-            println!("Shutting down.")
+    if let Some(limit) = config.requests_to_serve {
+        println!("Going to serve {limit} request(s) at most.");
+        for stream in listener.incoming().take(limit) {
+            let stream = stream.unwrap();
+            pool.execute(|| {
+                lib::handle_connection(stream);
+            });
         }
-        None => {
-            for stream in listener.incoming() {
-                let stream = stream.unwrap();
-                pool.execute(|| {
-                    lib::handle_connection(stream);
-                });
-            }
-        }
+        println!("Shutting down.");
+        return;
+    }
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            lib::handle_connection(stream);
+        });
     }
 }
